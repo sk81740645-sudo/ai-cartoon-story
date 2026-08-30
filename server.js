9a512188a61c24bc02462c503fcdc4731a4c74b0
sk81740645-sudo <sk81740645@gmail.com>
2026-08-30 17:58:02 +0530

    Server.js

--- server.js
+++ server.js
@@ -49,8 +49,7 @@ app.post("/api/chat", async (req, res) => {
     });
   }
 });
-
-app.get("*", (req, res) => {
+app.get('/*splat', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
